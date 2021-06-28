d5f3b037bcf799ba6cdecd5184491da5c6606988
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-28 20:06:37 +0200

    fino a lista contatti

--- js/script.js
+++ js/script.js
@@ -8,4 +8,49 @@
     // messages: ,
     // }]
     
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            contacts : [
+                {
+                    name: 'Tullio',
+                    avatar: 'img/avatar_1.jpg',
+                    visible: true,
+                },
+                {
+                    name: 'Giobanni',
+                    avatar: 'img/avatar_3.jpg',
+                    visible: true,
+                },
+                {
+                    name: 'Danielo',
+                    avatar: 'img/avatar_4.jpg',
+                    visible: true,
+                },
+                {
+                    name: 'Marcolino',
+                    avatar: 'img/avatar_5.jpg',
+                    visible: true,
+                },
+                {
+                    name: 'Barbra',
+                    avatar: 'img/avatar_6.jpg',
+                    visible: true,
+                },
+                {
+                    name: 'Jacobbe',
+                    avatar: 'img/avatar_7.jpg',
+                    visible: true,
+                },
+                {
+                    name:'Cristina',
+                    avatar: 'img/avatar_10.jpg',
+                    visible: true,
+                },
+            ]
+        }
+      })
+
+
     
