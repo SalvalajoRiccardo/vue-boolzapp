68e8c3fc1fe15d522b40ba8e35778fffd4ea890c
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-29 15:17:44 +0200

    fine milestone 2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
     var app = new Vue({
         el: '#app',
         data: {
-            contatore : 1,
+            contatore : 0,
             contacts : [
                 {
                     name: 'Tullio',
@@ -30,31 +30,124 @@
                     name: 'Giobanni',
                     avatar: 'img/avatar_3.jpg',
                     visible: false,
+                    messages: [
+                        {
+                            text: 'ciao, come va?',
+                            date: '11/04/2020 07:30:05',
+                            status: 'rec'
+                        },
+                        {
+                            text: 'ciao giobanni, bene dai, e tu?',
+                            date: '11/04/2020 07:37:50',
+                            status: 'sent'
+                        },
+                        {
+                            text: 'fa caldo',
+                            date: '11/04/2020 07:43:15',
+                            status: 'rec'
+                        },
+                    ]
                 },
                 {
                     name: 'Danielo',
                     avatar: 'img/avatar_4.jpg',
                     visible: false,
+                    messages: [
+                        {
+                            text: 'ciao, tutto bene con il vaccino?',
+                            date: '14/08/2020 20:30:17',
+                            status: 'sent'
+                        },
+                        {
+                            text: 'ho un po di male al braccio, ma niente altro',
+                            date: '15/08/2020 08:04:57',
+                            status: 'rec'
+                        },
+                        
+                    ]
                 },
                 {
                     name: 'Marcolino',
                     avatar: 'img/avatar_5.jpg',
                     visible: false,
+                    messages: [
+                        {
+                            text: 'ciao tulio, come va?',
+                            date: '10/01/2020 15:30:45',
+                            status: 'sent'
+                        },
+                        {
+                            text: 'hai sbagliato contatto',
+                            date: '11/01/2020 16:10:50',
+                            status: 'rec'
+                        },
+                    ]
                 },
                 {
                     name: 'Barbra',
                     avatar: 'img/avatar_6.jpg',
                     visible: false,
+                    messages: [
+                        {
+                            text: 'ciao, ricordati di portare i bicchieri domani',
+                            date: '10/11/2020 14:30:55',
+                            status: 'rec'
+                        },
+                        {
+                            text: 'ciao, si ora mi faccio un promemoria',
+                            date: '10/11/2020 17:30:50',
+                            status: 'sent'
+                        },
+                        {
+                            text: 'serve altro?',
+                            date: '10/11/2020 17:31:15',
+                            status: 'sent'
+                        },
+                    ]
                 },
                 {
                     name: 'Jacobbe',
                     avatar: 'img/avatar_7.jpg',
                     visible: false,
+                    messages: [
+                        {
+                            text: 'ciao jac, come va?',
+                            date: '09/06/2020 15:30:55',
+                            status: 'sent'
+                        },
+                        {
+                            text: 'tutto bene dai, e tu?',
+                            date: '09/06/2020 17:30:50',
+                            status: 'rec'
+                        },
+                        {
+                            text: 'hai cominicato a lavorare?',
+                            date: '09/06/2020 17:31:15',
+                            status: 'sent'
+                        },
+                        {
+                            text: 'si ho cominciato ieri',
+                            date: '09/06/2020 17:34:02',
+                            status: 'rec'
+                        },
+                    ]
                 },
                 {
                     name:'Cristina',
                     avatar: 'img/avatar_10.jpg',
                     visible: false,
+                    messages: [
+                        {
+                            text: 'ciao cristina, non ho capito, puoi rispiegare?',
+                            date: '28/06/2021 10:05:55',
+                            status: 'sent'
+                        },
+                        {
+                            text: 'certo, cosa non hai capito?',
+                            date: '28/06/2021 10:05:57',
+                            status: 'rec'
+                        },
+                    ]
                 },
             ]
         },
